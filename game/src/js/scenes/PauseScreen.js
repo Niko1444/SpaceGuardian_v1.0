7d7550fbc0fd554f3529fe7f42af9a5114421914
meomun1 <maluong1504@gmail.com>
2023-12-07 23:17:58 +0700

    Update GUI Manager included basic

--- game/src/js/scenes/PauseScreen.js
+++ game/src/js/scenes/PauseScreen.js
@@ -1,6 +1,7 @@
 import Phaser from "phaser";
 import config from "../config/config";
 import KeyboardManager from "../manager/KeyboardManager";
+import GuiManager from "../manager/uiManager";
 class PauseScreen extends Phaser.Scene {
   constructor() {
     super("pauseScreen");
@@ -8,32 +9,7 @@ class PauseScreen extends Phaser.Scene {
 
   create() {
     this.keyboardManager = new KeyboardManager(this);
-
-    const pauseText = this.add.text(
-      config.width / 2,
-      config.height / 2 - 50,
-      "Pause",
-      { fontSize: "32px", fill: "#fff" }
-    );
-    pauseText.setOrigin(0.5);
-
-    // Add instructions to restart the game
-    const unpauseText = this.add.text(
-      config.width / 2,
-      config.height / 2,
-      "Press P to Unpause",
-      { fontSize: "24px", fill: "#fff" }
-    );
-    unpauseText.setOrigin(0.5);
-
-    // Add instructions to back to the title screen
-    const backToTitleText = this.add.text(
-      config.width / 2,
-      config.height / 2 + 30,
-      "Press T to TitleScreen",
-      { fontSize: "24px", fill: "#fff" }
-    );
-    backToTitleText.setOrigin(0.5);
+    this.guiManager = new GuiManager(this);
 
     this.keyboardManager.unpauseGame();
 
