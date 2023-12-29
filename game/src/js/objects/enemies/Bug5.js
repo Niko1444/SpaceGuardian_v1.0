7d4f0ecb19ea47f1d2514b9cbd04ae44d4e52677
luan <latla21@gnail.com>
2023-12-29 12:37:23 +0700

    final wave for level 2

--- game/src/js/objects/enemies/Bug5.js
+++ game/src/js/objects/enemies/Bug5.js
@@ -5,27 +5,34 @@ import config from "../../config/config";
 import UpgradeManager from "../../manager/UpgradeManager";
 
 class Bug5 extends Entity {
-  constructor(scene, x, y, health) {
+  constructor(scene, x, y, health, scale = 1) {
     super(scene, x, y, "bug5_texture", health);
+
+    // Set the scale of the bug sprite
+    this.setScale(scale);
+
     this.body.velocity.y = gameSettings.enemySpeed;
     this.health = health;
     this.maxHealth = health;
-    this.damage = 100;
     this.hpBarWidth = 20;
     this.hpBarHeight = 5;
+    this.damage = 100;
     this.setInteractiveEntity();
+
+    const scaledHpBarWidth = this.hpBarWidth * scale;
+    const scaledHpBarHeight = this.hpBarHeight * scale;
 
     this.hpBar = new HPBar(
       scene,
       this.x,
-      this.y,
-      this.hpBarWidth,
-      this.hpBarHeight,
+      this.y - scaledHpBarHeight - 5, 
+      scaledHpBarWidth,
+      scaledHpBarHeight,
       this.health,
       this.maxHealth
     );
     this.scene.add.existing(this.hpBar);
-  }
+}
 
   setVelocityY(velocity) {
     super.setVelocityY(velocity);
