124de946891cf3d0b6a3d2381813b42f9fb1ecdb
luan <latla21@gnail.com>
2023-12-30 15:40:24 +0700

    edit credit screen

--- game/src/js/scenes/CreditScreen.js
+++ game/src/js/scenes/CreditScreen.js
@@ -10,15 +10,18 @@ class CreditsScene extends Phaser.Scene {
       this.cameras.main.setBackgroundColor('#000'); // Set the background color to black
   
       let creditsText = `
-        Director: John Doe
-        Producer: Jane Doe
-        Lead Programmer: Alice
-        Art Director: Bob
-        Music: Charlie
-        Special Thanks: Our Players
+        SPACE GUARDIAN
+        TEAM: FLY TEAM
+        LEADER:
+        Tien Phat
+        MEMBERS:
+        Minh Luong 
+        Giang Nguyen 
+        Tien Luan
+        Special Thanks for playing our game!
       `;
   
-      let text = this.add.text(0, this.cameras.main.height, creditsText, { color: '#fff', align: 'center' });
+      let text = this.add.text(0, this.cameras.main.height, creditsText, { color: '#fff', align: 'center', fontFamily: "Pixelify Sans", fontSize: '32px' });
       text.x = this.cameras.main.width / 2 - text.width / 2; // Center the text
   
       // Create a tween to move the text to the top of the screen
